9e8c882832af20da637fabb085662e34b24570ac
Nicholas Lurski <nlurski@gmail.com>
2019-01-24 19:24:44 -0500

    need to fix perms

---
+++ LipReader.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace LipReader.Droid
+{
+    [Activity(Label = "LipReader", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            int requestPermissions=0;
+            string[] cameraPermission = { Android.Manifest.Permission.Camera };
+            RequestPermissions(cameraPermission, requestPermissions);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
